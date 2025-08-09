d194d166a80f65f535347ce3bad0dada36743b46
Rajat Shetty <rajatshetty982@gmail.com>
2025-08-09 22:56:41 +0530

    Enable nerd font and relative num

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.gave_nerd_font = false
+vim.g.gave_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -16,7 +16,7 @@ vim.g.gave_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
